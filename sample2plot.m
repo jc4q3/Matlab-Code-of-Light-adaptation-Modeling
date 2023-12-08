4078a8157ee68b75017a90737ba55f4882678803
photosynthesis <70293090+jc4q3@users.noreply.github.com>
2023-12-08 14:22:41 -0600

    Add files via upload

--- sample2plot.m
+++ sample2plot.m
@@ -8,62 +8,55 @@ tt=[dtt1:dtt1:1,1+dtt2:dtt2:300,300+dtt1:dtt1:301,301+dtt2:dtt2:331,331+dtt1:dtt
 Ydata=[Y(v);Y(1001:30900);Y(30900+v);Y(31901:34900);Y(34900+v);Y(35901:38900);Y(38900+v);Y(39901:42900);Y(42900+v)]';
 samplinginterval=0.01;
 t=samplinginterval:samplinginterval:394;
-k=[0.00167329
-17.26766002
-272.7647867
-1.593929845
-505.9804451
-4195.417523
-935.2050085
-627.9329292
-466.4993774
-59.85373973
-5.241172674
-163.7998062
-46.53180339
-6.140335165
-0.185126133
-59.66272278
-4.142028365
-0.008970589
-0.044584729
-5.325417479
-1.729445654
-47.00912917
-4.786692643
-129.8735435
-1.146396523
-1.616404233
-1.49104289
-1.218063986
-30.96345315
-43.01051674
-11.28942854
-410.6944121
+k=[0.00512739
+37.0634972
+4132.057485
+2383.437775
+2295.036611
+4798.05409
+2991.68901
+1390.613943
+799.4098187
+492.116175
+5.241172
+13.188225
+162.216
+46.63351
+18.62785812
+19.1549208
+34.23285257
+2.823629103
+0.043870139
+0.039855617
+0.006608619
+0.093054
+1.626352379
+1299.509
+1.147553397
+1.616404
+74.95251815
+1.218064
+5.272081957
+137.0297
+187.139084
+218.0447831
+11.7365855
+5.373113196
 ];
 
-[Fluo,x,beta,R_psbs,R_VDE,delta_psi,delta_pH_part,pmf]=PulseVariedflueqnRK4_PSBS(k,t);
+[Fluo,x,beta,R_psbs,R_VDE,delta_psi,delta_pH_part,pmf,N]=PulseVariedflueqnRK4_PSBS(k,t);
 Yold=Fluo;
 figure(1)
 plot(t,Ydata);
-% ylim([40 500])
+
 hold on
 plot(t,Yold);
 legend('Fluorescence experimental data','Fluorescence estimated');
 xlabel('time');
 ylabel('Fluorescence');
 
+
 figure(2)
-semilogx(t,Ydata);
-% ylim([40 500])
-hold on
-semilogx(t,Yold);
-legend('Fluorescence experimental data','Fluorescence estimated');
-xlabel('time');
-ylabel('Fluorescence');
-
-
-figure(3)
 subplot(3,3,1)
 plot(t,x(1,:))
 xlabel('time');
@@ -102,7 +95,7 @@ xlabel('time');
 ylabel('R_V_D_E');
 
 
-figure(4)
+figure(3)
 subplot(3,3,1)
 plot(t,x(8,:))
 xlabel('time');
@@ -139,6 +132,11 @@ subplot(3,3,9)
 plot(t,pmf)
 xlabel('time');
 ylabel('pmf');
+
+figure
+plot(t,N)
+xlabel('time');
+ylabel('r14');
 
 n=length(Ydata);
 MAPE=1/n*sum(abs((Ydata-Yold)./Yold))*100
